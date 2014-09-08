68224bcec17e43e1967c27c3dbc2f5fd8fefca72
kjirou <kjirou.web@gmail.com>
2014-09-08 19:12:52 +0900

    Add a sample of "exports"

---
+++ basis/exports/index.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+//
+// exports は辞書としての object を直接返す
+//
+
+assert = require('assert');
+foo = require('./foo');  // 別ファイルで exports = しているもの
+
+
+assert.deepEqual(foo, {
+  x: 1,
+  y: 2
+});
