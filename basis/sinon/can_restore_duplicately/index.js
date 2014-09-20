96f744eca16db6e2597925cac5e6313a24b386b6
kjirou <kjirou.web@gmail.com>
2014-09-20 19:28:56 +0900

    Add a sinon sample

---
+++ basis/sinon/can_restore_duplicately/index.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+var assert = require('assert');
+var sinon = require('sinon');
+
+
+obj = {
+  foo: function(){ return 1; }
+};
+assert(obj.foo() === 1);
+
+objMock = sinon.stub(obj, 'foo', function(){ return 2; });
+assert(obj.foo() === 2);
+
+objMock.restore();
+assert(obj.foo() === 1);
+
+// 二重に restore できる
+objMock.restore();
