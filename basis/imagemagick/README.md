c6389448fcb5db4222d79b1295872054f07841c3
kjirou <kjirou.web@gmail.com>
2014-11-10 01:09:31 +0900

    Add a sample of imagemagick

---
+++ basis/imagemagick/README.md
@@ -0,0 +1,22 @@
+## Preparation
+### Install imagemagick
+This npm-module depends on [imagemagick](http://www.imagemagick.org/) CLI tools.
+
+e.g. Mac with Homebrew:
+```
+brew install imagemagick
+```
+
+e.g. Debian:
+```
+sudo apt-get install imagemagick
+```
+
+Assert ImageMagick is installed:
+```
+$convert --version
+Version: ImageMagick 6.8.7-7 Q16 x86_64 2013-11-27 http://www.imagemagick.org
+Copyright: Copyright (C) 1999-2014 ImageMagick Studio LLC
+Features: DPC Modules
+Delegates: bzlib freetype jng jpeg ltdl png xml zlib
+```
---
+++ basis/imagemagick/convert-crop/index.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+var assert = require('assert');
+var im = require('imagemagick');
+
+
+// txt: で対象範囲の情報をテキストで取得
+im.convert([
+  '../support/denzi/Denzi140330-12.png', '-crop', '1x1+0+0', 'txt:'
+], function(err, stdout, stderr){
+  assert.ifError(err);
+  assert.strictEqual(stdout,
+    '# ImageMagick pixel enumeration: 1,1,255,srgba\n' +
+    '0,0: (0,0,0,1)  #000000  black\n'
+  );
+});
+
+// 指定範囲を別画像として生成する
+// 範囲指定の書式は {width}x{height}+{top}+{left}
+im.convert([
+  '../support/denzi/Denzi140330-12.png', '-crop', '16x16+0+352', 'tmp/fighter.png'
+], function(err, stdout, stderr){
+  assert.ifError(err);
+  // 標準出力は空
+  assert.strictEqual(stdout, '');
+
+  // fighter.png が生成されている
+  im.identify('tmp/fighter.png', function(err, features){
+    assert.ifError(err);
+    assert.strictEqual(features.width, 16);
+    assert.strictEqual(features.height, 16);
+  });
+});
