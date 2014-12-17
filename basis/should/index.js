cdd3a9e4281004df5ea3690ddca0966c10107518
kjirou <kjirou.web@gmail.com>
2014-12-17 14:57:33 +0900

    Update a should.js sample

--- basis/should/index.js
+++ basis/should/index.js
@@ -4,15 +4,27 @@ var assert = require('assert');
 var should = require('should');
 
 
-// eql は == や deepEqual 的な判定
-(true).should.eql(1);
+//
+// eql は deepEqual 的な判定
+//
+// ただ、判定は参照一致ではないけど自動キャストはしなくなった
+// つまり、true と 1 は一致しない。これは 4.1-4.3 の間の何処かで変わった
+//
+(true).should.eql(true);
+assert.throws(function(){
+  (true).should.eql(1);
+}, /AssertionError/);
+(NaN).should.eql(NaN);
 [1, 2, 3].should.eql([1, 2, 3]);
 ('foo').should.eql('foo');
 assert.throws(function(){
   ('foo').should.eql('bar');
 }, /AssertionError/);
 
+
+//
 // 完全一致は equal や be.exactly
+//
 (2).should.equal(2);
 ('foo').should.be.exactly('foo');
 assert.throws(function(){
