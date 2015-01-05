0432a1a1027ded0a023b00d8d128c89e92ebea67
kjirou <kjirou.web@gmail.com>
2015-01-05 17:34:50 +0900

    Add a selenium-webdriver sample

---
+++ basis/selenium-webdriver/execute-js-scripts/index.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+var assert = require('assert');
+var webdriver = require('selenium-webdriver');
+
+
+var driver = new webdriver.Builder()
+  .withCapabilities(webdriver.Capabilities.chrome())
+  .build();
+
+driver.get('http://example.com').then(function(){
+  return driver.getTitle();
+}).then(function(title){
+  assert.strictEqual(title, 'Example Domain');
+}).then(function(){
+  // executeScript を使う
+  // ちなみに、主にこれはJSエラーを拾う場合に必要になるが、その場合は
+  // window.onerror でフックする
+  return driver.executeScript('return ["a", "b", "c"];');
+}).then(function(result){
+  assert.strictEqual(result.join('|'), 'a|b|c');
+});
