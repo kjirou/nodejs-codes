bd799b5fbf8a5351ce4f19bbaffaa99cc98f31ba
kjirou <kjirou.web@gmail.com>
2015-07-24 19:42:03 +0900

    Add a memo

--- examples/rx/index.js
+++ examples/rx/index.js
@@ -8,6 +8,11 @@ var util = require('util');
 // Rx.Observable.timer
 //   https://github.com/Reactive-Extensions/RxJS/blob/master/doc/api/core/operators/timer.md
 //
+// Rx.Observable.prototype.pluck(property)
+//   https://github.com/Reactive-Extensions/RxJS/blob/master/doc/api/core/operators/pluck.md
+//
+// Note: Use ".from", if you will pass to static data
+//
 
 
 var args = process.argv.slice(2);
