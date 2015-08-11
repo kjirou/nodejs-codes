7267c4a03fe206611689dc9d55bc5a44a4086773
kjirou <kjirou.web@gmail.com>
2015-08-11 17:09:11 +0900

    Add a selenium-webdriver sample

---
+++ examples/selenium-webdriver/webdriver.By.css/index.js
@@ -0,0 +1,56 @@
+#!/usr/bin/env node
+
+//
+// Test `webdriver.By.css`
+//
+// http://selenium.googlecode.com/git/docs/api/javascript/namespace_webdriver_By.html#css
+//
+
+var assert = require('assert');
+var webdriver = require('selenium-webdriver');
+
+
+var TEST_PAGE_FILE_PATH = 'file://' + __dirname + '/test-page.html'
+
+var driver = new webdriver.Builder()
+  .withCapabilities(webdriver.Capabilities.chrome())
+  .build();
+
+webdriver.promise.fulfilled()
+  .then(function() {
+    return driver.get(TEST_PAGE_FILE_PATH);
+  })
+  .then(function() {
+    return driver.findElements(webdriver.By.css('input'));
+  })
+  .then(function(elements) {
+    console.log('"input" is', elements.length);
+  })
+  .then(function() {
+    return driver.findElements(webdriver.By.css('textarea'));
+  })
+  .then(function(elements) {
+    console.log('"textarea" is', elements.length);
+  })
+  .then(function() {
+    return driver.findElements(webdriver.By.css('input[type="number"]'));
+  })
+  .then(function(elements) {
+    console.log('"input[type="number"]" is', elements.length);
+  })
+  .then(function() {
+    return driver.findElements(webdriver.By.css('input[min="5"]'));
+  })
+  .then(function(elements) {
+    console.log('"input[min="5"]" is', elements.length);
+  })
+  .then(function() {
+    return driver.findElements(webdriver.By.css('input[type="number"][min="5"]'));
+  })
+  .then(function(elements) {
+    console.log('"input[type="number"][min="5"]" is', elements.length);
+  })
+  .then(function() {
+    driver.quit();
+  })
+;
