25b5f0341a50012090fe9d81ac297ce270868437
kjirou <kjirou.web@gmail.com>
2015-08-15 21:36:28 +0900

    Update

--- examples/uuid/index.js
+++ examples/uuid/index.js
@@ -3,5 +3,9 @@
 var assert = require('assert');
 var uuid = require('uuid');
 
-var id = uuid.v4();
-console.log(id);
+[0, 1, 2, 3, 4].forEach(function() {
+  // v4 だけ、機器や時間などに依存せず、完全にランダムな値から生成する
+  // Ref) http://e-words.jp/w/UUID.html
+  var id = uuid.v4();
+  console.log(id);
+});
