a5c1627f05c3cdc2276e1281aa3bf167c1c6801e
kjirou <kjirou.web@gmail.com>
2015-08-16 17:00:55 +0900

    Add a sample of events module

---
+++ examples/events/index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+var EventEmitter = require('events').EventEmitter;
+
+
+var emitter = new EventEmitter();
+emitter.on('foo', function() {
+  console.log('on foo');
+});
+
+var emitter2 = new EventEmitter();
+emitter.emit('foo');
