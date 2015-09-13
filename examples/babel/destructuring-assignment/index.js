d6d356c9382b2bc2efe5e278fc831892f5312983
kjirou <kjirou.web@gmail.com>
2015-09-14 00:06:35 +0900

    Update a sample of ES6

--- examples/babel/destructuring-assignment/index.js
+++ examples/babel/destructuring-assignment/index.js
@@ -3,6 +3,9 @@
 import assert from 'assert';
 
 
+//
+// 分割代入内で代入ができる
+//
 function getObj() {
   return {
     x: 2,
@@ -16,7 +19,15 @@ let {
   z = 6
 } = getObj();
 
-
 assert.strictEqual(x, 2);
 assert.strictEqual(y, 3);
 assert.strictEqual(z, 6);
+
+
+//
+// 別変数名で展開できる
+//
+function stringifyObj({ x: xVar, y: yVar }) {
+  return xVar + yVar;
+}
+assert.strictEqual(stringifyObj({ x: 'foo', y: 'bar' }), 'foobar');
