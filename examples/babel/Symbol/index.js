8c07549ea7f865a5af41f51abc23d386e9cf7a62
kjirou <kjirou.web@gmail.com>
2015-09-19 15:51:49 +0900

    Update

--- examples/babel/Symbol/index.js
+++ examples/babel/Symbol/index.js
@@ -3,15 +3,19 @@
 import assert from 'assert';
 
 
+//
+// private-like property
+//
+
 var NAME_SYMBOL = Symbol('name');
 var AGE_SYMBOL = Symbol('age');
-
 
 class Person {
 
   constructor(name, age) {
     this[NAME_SYMBOL] = name;
     this[AGE_SYMBOL] = age;
+    this.x = 1;
   }
 
   get name() {
@@ -27,3 +31,4 @@ class Person {
 let p = new Person('taro', 12);
 assert.strictEqual(p.name, 'taro');
 assert.strictEqual(p.age, 12);
+assert.deepEqual(Object.keys(p), ['x']);  // Not enumerable
