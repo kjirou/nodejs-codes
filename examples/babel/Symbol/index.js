ebd685aa6d577d59b28e642f17598770583526b5
kjirou <kjirou.web@gmail.com>
2015-09-19 13:02:59 +0900

    Add a sample

---
+++ examples/babel/Symbol/index.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env babel-node
+
+import assert from 'assert';
+
+
+var NAME_SYMBOL = Symbol('name');
+var AGE_SYMBOL = Symbol('age');
+
+
+class Person {
+
+  constructor(name, age) {
+    this[NAME_SYMBOL] = name;
+    this[AGE_SYMBOL] = age;
+  }
+
+  get name() {
+    return this[NAME_SYMBOL];
+  }
+
+  get age() {
+    return this[AGE_SYMBOL];
+  }
+}
+
+
+let p = new Person('taro', 12);
+assert.strictEqual(p.name, 'taro');
+assert.strictEqual(p.age, 12);
