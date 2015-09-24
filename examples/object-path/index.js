69ddcce19dfdc78ee79509e8aaf10a7e91eafe30
kjirou <kjirou.web@gmail.com>
2015-09-24 20:31:49 +0900

    Update a sample of object-path

--- examples/object-path/index.js
+++ examples/object-path/index.js
@@ -18,3 +18,15 @@ var obj = {
 assert.strictEqual(objectPath.get(obj, 'foo'), 1);
 assert.strictEqual(objectPath.get(obj, 'bar.y.1'), 200);
 assert.strictEqual(objectPath.get(obj, 'baz'), undefined);
+assert.strictEqual(objectPath.get(obj, 'baz.a.b.c'), undefined);
+
+assert.strictEqual(objectPath.has(obj, 'foo'), true);
+assert.strictEqual(objectPath.has(obj, 'bar.y.0'), true);
+assert.strictEqual(objectPath.has(obj, 'baz'), false);
+assert.strictEqual(objectPath.has(obj, 'baz.a.b.c'), false);
+
+objectPath.set(obj, 'foo', 100);
+assert.strictEqual(objectPath.get(obj, 'foo'), 100);
+
+objectPath.set(obj, 'baz.a.b.c', 'not_exists');
+assert.strictEqual(objectPath.has(obj, 'baz.a.b.c'), true);
