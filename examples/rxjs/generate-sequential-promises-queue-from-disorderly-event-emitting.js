8865781a6fb44d5669730e085f83cdbd46176f3b
kjirou <kjirou.web@gmail.com>
2016-11-19 01:56:43 +0900

    Update a sample

--- examples/rxjs/generate-sequential-promises-queue-from-disorderly-event-emitting.js
+++ examples/rxjs/generate-sequential-promises-queue-from-disorderly-event-emitting.js
@@ -5,7 +5,6 @@ const EventEmitter = require('events').EventEmitter;
 const Rx = require('rxjs/Rx');
 
 //
-// TODO: エラーが発生したら後続のイベントはキャンセルする（キューに残ってるイベントは消化する
 // TODO: toPromise の使い方がわからない
 //
 
@@ -15,24 +14,39 @@ class MyEventEmitter extends EventEmitter {
 const emitter = new MyEventEmitter();
 
 
-let source;
-
 //
 // fromEvent だと 'event1' しか listen できない
 // https://github.com/Reactive-Extensions/RxJS/blob/master/doc/api/core/operators/fromeventpattern.md
 //
-//source = Rx.Observable.fromEvent(emitter, 'event1');
-source = Rx.Observable.fromEventPattern((rxCallback) => {
-  emitter.on('event1', rxCallback);
-  emitter.on('event2', rxCallback);
-});
+//const source = Rx.Observable.fromEvent(emitter, 'event1');
+const source = Rx.Observable
+  .fromEventPattern(
+    (rxCallback) => {
+      emitter.addListener('event1', rxCallback);
+      emitter.addListener('event2', rxCallback);
+    },
+    (rxCallback) => {
+      // TODO:
+      // removeListener するらしい
+    }
+  )
+;
 
-source
+// RxJS 5 からの pausable 実装について
+// Ref) https://github.com/ReactiveX/rxjs/issues/1542#issuecomment-202540367
+const pauser = new Rx.Subject();
+
+const pausableSource = pauser.switchMap(paused => paused ? Rx.Observable.never() : source);
+
+//const stream = source
+const stream = pausableSource
   .concatMap(payload => {
     const promise = new Promise((resolve, reject) => {
       setTimeout(() => {
         if (payload.error) {
           reject(payload.error);
+        } else if (payload.paused) {
+          resolve(payload);
         } else {
           resolve(payload);
         }
@@ -43,17 +57,18 @@ source
 ;
 
 
-let subscription = source
+const subscription = stream
   .subscribe(
     // success
     (payload) => {
       console.log('success:', payload);
     },
-    // error
+    // error　エラーハンドリング、一度でもエラーを流すとここに入って終わる
+    // Ref) http://qiita.com/bouzuya/items/5e068659d63d9961a260
     (payload) => {
       console.log('error:', payload);
     },
-    // TODO: 何か実行されないけどいいや
+    // TODO: 何か実行されないけどいいや、fromEvent だから終わりがないのかも
     // completed
     () => {
       console.log('completed');
@@ -65,13 +80,16 @@ let subscription = source
   //.catch(payload => console.error('error:', payload))
 ;
 
+pauser.next(false);
+
 
 // 登録順で、常に 1 処理だけ、実行される
+emitter.emit('event1', { waitTime: 500 });
+emitter.emit('event1', { waitTime: 1000 });
+emitter.emit('event2', { waitTime: 501 });
 emitter.emit('event1', { waitTime: 100 });
 emitter.emit('event1', { waitTime: 500 });
-emitter.emit('event2', { waitTime: 201 });
-emitter.emit('event1', { waitTime: 100 });
-emitter.emit('event1', { waitTime: 300 });
-emitter.emit('event2', { waitTime: 401 });
+emitter.emit('event2', { waitTime: 1001 });
+//pauser.next(true);  // この外部入力により止められる
 emitter.emit('event1', { waitTime: 50, error: new Error('XXX!!!') });
 emitter.emit('event1', { waitTime: 600 });
